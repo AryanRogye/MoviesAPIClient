72c12d9ea7ab64bb2b26f27d5e7e8a3e61d3ae77
AryanRogye <aryan.rogye@gmail.com>
2026-09-14 17:36:07 -0500

    first commit

---
+++ MoviesAPIClient/MoviesAPIClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAPIClientApp.swift
+//  MoviesAPIClient
+//
+//  Created by Aryan Rogye on 9/14/26.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAPIClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoviesAPIClientTests/MoviesAPIClientTests.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesAPIClientTests.swift
+//  MoviesAPIClientTests
+//
+//  Created by Aryan Rogye on 9/14/26.
+//
+
+import Testing
+
+struct MoviesAPIClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
